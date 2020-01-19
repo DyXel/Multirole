4b0d9aa11a8854fcca13c6ae830846864f835777
kevinlul <6320810+kevinlul@users.noreply.github.com>
2020-01-19 09:18:34 -0500

    Init CMake, AGPL3

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+/**
+ *  Project Ignis: Multirole
+ *  Copyright (C) 2020  edo9300, DyXel, kevinlul
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+ 
+#include <string>
+
+#if defined(UNICODE)
+int wmain(int argc, wchar_t *argv[]) {
+#else
+int main(int argc, char *argv[]) {
+#endif
+	return EXIT_SUCCESS;
+}
