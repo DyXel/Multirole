67c7b36fa79907fff3ba8a3c2a5541dca4149f40
Dylam De La Torre <dyxel04@gmail.com>
2020-01-22 23:58:15 +0100

    prevent webhooks from passing non-nullterminated strings to callback

--- src/WebhookEndpoint.cpp
+++ src/WebhookEndpoint.cpp
@@ -29,9 +29,10 @@ void WebhookEndpoint::DoAccept()
 	acceptor.async_accept(
 	[this](const std::error_code& ec, asio::ip::tcp::socket soc)
 	{
+		if(ec)
+			return;
 		DoAccept();
-		if(!ec)
-			DoReadHeader(std::move(soc));
+		DoReadHeader(std::move(soc));
 	});
 }
 
@@ -39,16 +40,16 @@ void WebhookEndpoint::DoReadHeader(asio::ip::tcp::socket soc)
 {
 	using namespace asio::ip;
 	auto socPtr = std::make_shared<tcp::socket>(std::move(soc));
-	auto payload = std::make_shared<std::string>(' ', 256);
+	auto payload = std::make_shared<std::string>(' ', 255);
 	socPtr->async_read_some(asio::buffer(*payload),
-	[this, socPtr, payload](std::error_code ec, std::size_t)
+	[this, socPtr, payload](const std::error_code& ec, std::size_t)
 	{
-		if(!ec)
-		{
-			asio::write(*socPtr, asio::buffer("HTTP/1.0 200 OK\r\n"));
-			std::error_code ignoredEc;
-			socPtr->shutdown(tcp::socket::shutdown_both, ignoredEc);
-		}
+		if(ec)
+			return;
+		asio::write(*socPtr, asio::buffer("HTTP/1.0 200 OK\r\n"));
+		std::error_code ignoredEc;
+		socPtr->shutdown(tcp::socket::shutdown_both, ignoredEc);
+		*payload += '\0'; // Guarantee null-terminated string
 		Callback(*payload);
 	});
 }
