b98ca5a1c4d91bba7cfc77ab2d96ed63584a3d67
Dylam De La Torre <dyxel04@gmail.com>
2020-01-24 03:08:41 +0100

    Add StringUtils for easy conversion between the used locales

---
+++ src/StringUtils.cpp
@@ -0,0 +1,40 @@
+#include "StringUtils.hpp"
+
+#include <codecvt>
+#include <locale>
+
+namespace Ignis
+{
+
+namespace StringUtils
+{
+
+std::u16string BufferToUTF16(void* data, std::size_t maxCount)
+{
+	auto p = reinterpret_cast<char16_t*>(data), p2 = p;
+	std::size_t ntPos = 0u; // Null terminator position.
+	while(ntPos < maxCount && *p != 0u)
+	{
+		p++;
+		ntPos++;
+	}
+	return std::u16string(p2, ntPos);
+}
+
+
+#define WCI std::wstring_convert<std::codecvt_utf8<char16_t>, char16_t>{}
+std::string UTF16ToUTF8(std::u16string_view str)
+{
+	return WCI.to_bytes(str.data());
+}
+
+std::u16string UTF8ToUTF16(std::string_view str)
+{
+	return WCI.from_bytes(str.data());
+}
+#undef WCI
+
+} // namespace StringUtil
+
+} // namespace Ignis
+
---
+++ src/StringUtils.hpp
@@ -0,0 +1,20 @@
+#ifndef STRINGUTILS_HPP
+#define STRINGUTILS_HPP
+#include <string>
+
+namespace Ignis
+{
+
+namespace StringUtils
+{
+
+std::string UTF16ToUTF8(std::u16string_view str);
+std::u16string UTF8ToUTF16(std::string_view str);
+
+std::u16string BufferToUTF16(void* data, std::size_t maxCount);
+
+} // namespace StringUtil
+
+} // namespace Ignis
+
+#endif // STRINGUTILS_HPP
