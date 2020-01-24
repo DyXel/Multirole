85b6d1acf1ce01b2aca140f8385739ff834c3631
Dylam De La Torre <dyxel04@gmail.com>
2020-01-25 00:58:51 +0100

    add easy way to retrieve the correct structure for CTOS messages

--- src/CTOSMsg.hpp
+++ src/CTOSMsg.hpp
@@ -1,5 +1,6 @@
 #ifndef CTOSMSG_HPP
 #define CTOSMSG_HPP
+#include <cstdint>
 
 namespace YGOPro
 {
@@ -34,6 +35,40 @@ public:
 		HS_START      = 0x25
 	};
 
+	struct HostInfo
+	{
+		uint32_t lflist;
+		uint8_t rule;
+		uint8_t mode;
+		uint8_t duel_rule;
+		uint8_t no_check_deck;
+		uint8_t no_shuffle_deck;
+		uint32_t start_lp;
+		uint8_t start_hand;
+		uint8_t draw_count;
+		uint16_t time_limit;
+		uint64_t handshake;
+		int32_t team1;
+		int32_t team2;
+		int32_t best_of;
+		uint32_t duel_flag;
+		int32_t forbiddentypes;
+		uint16_t extra_rules;
+	};
+
+	struct PlayerInfo
+	{
+		uint16_t name[20];
+	};
+
+	struct CreateGame
+	{
+		HostInfo info;
+		uint16_t name[20];
+		uint16_t pass[20];
+		char notes[200];
+	};
+
 	CTOSMsg() : r(data + HEADER_LENGTH)
 	{}
 
@@ -60,15 +95,6 @@ public:
 		return true;
 	}
 
-	template<typename T>
-	const T Read()
-	{
-		T v;
-		std::memcpy(&v, r, sizeof(T));
-		r += sizeof(T);
-		return v;
-	}
-
 	uint8_t* Data()
 	{
 		return data;
@@ -78,6 +104,20 @@ public:
 	{
 		return data + HEADER_LENGTH;
 	}
+
+#define X(s) \
+	std::pair<bool, s> Get##s() const \
+	{ \
+		std::pair<bool, s> p; \
+		if(GetLength() != sizeof(s)) \
+			return p; \
+		p.first = true; \
+		std::memcpy(&p.second, data + HEADER_LENGTH, sizeof(s)); \
+		return p; \
+	}
+	X(PlayerInfo)
+	X(CreateGame)
+#undef X
 
 private:
 	uint8_t data[HEADER_LENGTH + MSG_MAX_LENGTH];
