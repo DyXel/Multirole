fe59d5b4eb1c71460c3fe33403dda7b314f4889a
Dylam De La Torre <dyxel04@gmail.com>
2020-01-25 23:28:58 +0100

    implement client message sending and receiving

--- src/Client.cpp
+++ src/Client.cpp
@@ -5,7 +5,8 @@
 namespace Ignis
 {
 
-namespace Multirole {
+namespace Multirole
+{
 
 Client::Client(IClientManager& owner, Properties initial, asio::ip::tcp::socket soc) :
 	owner(owner),
@@ -13,6 +14,75 @@ Client::Client(IClientManager& owner, Properties initial, asio::ip::tcp::socket 
 	soc(std::move(soc))
 {
 	owner.Add(shared_from_this());
+	DoReadHeader();
+}
+
+void Client::Send(const YGOPro::STOCMsg& msg)
+{
+	std::lock_guard<std::mutex> lock(m);
+	const bool WriteInProgress = !outgoing.empty();
+	outgoing.push(msg);
+	if(!WriteInProgress)
+		DoWrite();
+}
+
+void Client::DoReadHeader()
+{
+	auto buffer = asio::buffer(incoming.Data(), YGOPro::CTOSMsg::HEADER_LENGTH);
+	asio::async_read(soc, buffer,
+	[this](const std::error_code& ec, std::size_t)
+	{
+		if(!ec && incoming.IsHeaderValid())
+			DoReadBody();
+		else
+			owner.Remove(shared_from_this());
+	});
+}
+
+void Client::DoReadBody()
+{
+	auto buffer = asio::buffer(incoming.Body(), incoming.GetLength());
+	asio::async_read(soc, buffer,
+	[this](const std::error_code& ec, std::size_t)
+	{
+		if(!ec)
+		{
+			// Unlike RoomHostingEndpoint, we dont want to finish connection
+			// if the message is not properly handled. Just ignore it.
+			HandleMsg();
+			DoReadHeader();
+		}
+		else
+		{
+			owner.Remove(shared_from_this());
+		}
+	});
+}
+
+void Client::DoWrite()
+{
+	const auto& front = outgoing.front();
+// 	auto buffer = asio::buffer(front.Data(), front.Length());
+	asio::async_write(soc, asio::buffer(front.Data(), front.Length()),
+	[this](const std::error_code& ec, std::size_t)
+	{
+		if (!ec)
+		{
+			std::lock_guard<std::mutex> lock(m);
+			outgoing.pop();
+			if (!outgoing.empty())
+				DoWrite();
+		}
+		else
+		{
+			owner.Remove(shared_from_this());
+		}
+	});
+}
+
+void Client::HandleMsg()
+{
+	// TODO
 }
 
 } // namespace Multirole
--- src/Client.hpp
+++ src/Client.hpp
@@ -1,11 +1,17 @@
 #ifndef CLIENT_HPP
 #define CLIENT_HPP
+#include <queue>
+#include <mutex>
 #include <asio.hpp>
+
+#include "CTOSMsg.hpp"
+#include "STOCMsg.hpp"
 
 namespace Ignis
 {
 
-namespace Multirole {
+namespace Multirole
+{
 
 class IClientManager;
 
@@ -18,10 +24,21 @@ public:
 // 		Deck deck;
 	};
 	Client(IClientManager& owner, Properties initial, asio::ip::tcp::socket soc);
+
+	void Send(const YGOPro::STOCMsg& msg);
 private:
 	IClientManager& owner;
 	Properties properties;
 	asio::ip::tcp::socket soc;
+	YGOPro::CTOSMsg incoming;
+	std::queue<YGOPro::STOCMsg> outgoing;
+	std::mutex m;
+
+	void DoReadHeader();
+	void DoReadBody();
+	void DoWrite();
+
+	void HandleMsg();
 };
 
 } // namespace Multirole
