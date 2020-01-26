de72aa28b38dfceb6957bdff13fe75c59c214e61
Dylam De La Torre <dyxel04@gmail.com>
2020-01-26 01:50:20 +0100

    rename variables and note unused ones for YGOPro::HostInfo

--- src/MsgCommon.hpp
+++ src/MsgCommon.hpp
@@ -7,23 +7,23 @@ namespace YGOPro
 
 struct HostInfo
 {
-	uint32_t lflist;
-	uint8_t rule;
-	uint8_t mode;
-	uint8_t duel_rule;
-	uint8_t no_check_deck;
-	uint8_t no_shuffle_deck;
-	uint32_t start_lp;
-	uint8_t start_hand;
-	uint8_t draw_count;
-	uint16_t time_limit;
-	uint64_t handshake;
-	int32_t team1;
-	int32_t team2;
-	int32_t best_of;
-	uint32_t duel_flag;
-	int32_t forbiddentypes;
-	uint16_t extra_rules;
+	uint32_t banlistHash;
+	uint8_t scope; // OCG/TCG, etc
+	uint8_t mode; // NOTE: UNUSED
+	uint8_t duelRule; // NOTE: UNUSED
+	uint8_t dontCheckDeck;
+	uint8_t dontShuffleDeck;
+	uint32_t startingLP;
+	uint8_t startingDrawCount;
+	uint8_t drawCountPerTurn;
+	uint16_t timeLimitInSeconds;
+	uint64_t serverHandshake;
+	int32_t t1Count;
+	int32_t t2Count;
+	int32_t bestOf;
+	uint32_t duelFlags;
+	int32_t forbiddenTypes;
+	uint16_t extraRules; // Sealed Duel, Destiny Draw, etc
 };
 
 } // namespace YGOPro
