fbc9d5781269664e08f679bb63e694b2bf2e2055
Dylam De La Torre <dyxel04@gmail.com>
2020-04-03 15:06:21 +0200

    Add more checks to YGOPro::Deck

--- src/Multirole/YGOPro/Deck.cpp
+++ src/Multirole/YGOPro/Deck.cpp
@@ -5,10 +5,11 @@
 namespace YGOPro
 {
 
-Deck::Deck(CodeVector m, CodeVector e, CodeVector s) :
+Deck::Deck(CodeVector&& m, CodeVector&& e, CodeVector&& s, uint32_t err) :
 	main(std::move(m)),
 	extra(std::move(e)),
-	side(std::move(s))
+	side(std::move(s)),
+	error(err)
 {
 	auto MergeToMap = [&](const CodeVector& cv)
 	{
@@ -25,22 +26,6 @@ Deck::Deck(CodeVector m, CodeVector e, CodeVector s) :
 	MergeToMap(side);
 }
 
-bool Deck::Check(const Banlist& banlist) const
-{
-	for(const auto& kv : codeCounts)
-	{
-		if(banlist.IsWhitelist() && banlist.Whitelist().count(kv.first) == 0)
-			return false;
-		if(banlist.Forbidden().count(kv.first) != 0U)
-			return false;
-		if(kv.second > 1 && (banlist.Limited().count(kv.first) != 0U))
-			return false;
-		if(kv.second > 2 && (banlist.Semilimited().count(kv.first) != 0U))
-			return false;
-	}
-	return true;
-}
-
 const CodeVector& Deck::Main() const
 {
 	return main;
@@ -54,6 +39,51 @@ const CodeVector& Deck::Extra() const
 const CodeVector& Deck::Side() const
 {
 	return side;
+}
+
+uint32_t Deck::Error() const
+{
+	return error;
+}
+
+DeckType Deck::Check(const DeckLimits& limits) const
+{
+	auto OutOfBound = [](const auto& lim, std::size_t v) -> bool
+	{
+		return v < lim.min || v > lim.max;
+	};
+	if(OutOfBound(limits.main, main.size()))
+		return DECK_TYPE_MAIN;
+	if(OutOfBound(limits.extra, extra.size()))
+		return DECK_TYPE_EXTRA;
+	if(OutOfBound(limits.side, side.size()))
+		return DECK_TYPE_SIDE;
+	return DECK_TYPE_NONE;
+}
+
+uint32_t Deck::Check(const Banlist& banlist) const
+{
+	for(const auto& kv : codeCounts)
+	{
+		uint32_t code = kv.first;
+		if(banlist.IsWhitelist() && banlist.Whitelist().count(code) == 0)
+			return code;
+		if(banlist.Forbidden().count(code) != 0U)
+			return code;
+		if(kv.second > 1 && (banlist.Limited().count(code) != 0U))
+			return code;
+		if(kv.second > 2 && (banlist.Semilimited().count(code) != 0U))
+			return code;
+	}
+	return 0;
+}
+
+uint32_t Deck::MoreThan3() const
+{
+	for(const auto& kv : codeCounts)
+		if(kv.second > 3)
+			return kv.first;
+	return 0;
 }
 
 } // namespace YGOPro
--- src/Multirole/YGOPro/Deck.hpp
+++ src/Multirole/YGOPro/Deck.hpp
@@ -9,22 +9,46 @@ namespace YGOPro
 
 using CodeVector = std::vector<uint32_t>;
 
+enum DeckType : uint32_t
+{
+	DECK_TYPE_NONE = 0,
+	DECK_TYPE_MAIN,
+	DECK_TYPE_EXTRA,
+	DECK_TYPE_SIDE
+};
+
+struct DeckLimits;
 class Banlist;
 
 class Deck
 {
-	Deck(CodeVector m, CodeVector e, CodeVector s);
+public:
+	Deck(CodeVector&& m, CodeVector&& e, CodeVector&& s, uint32_t err);
 
-	bool Check(const Banlist& banlist) const;
 	const CodeVector& Main() const;
 	const CodeVector& Extra() const;
 	const CodeVector& Side() const;
+	uint32_t Error() const;
+
+	// Checks
+	DeckType Check(const DeckLimits& limits) const;
+	uint32_t Check(const Banlist& banlist) const;
+	uint32_t MoreThan3() const;
 private:
 	CodeVector main;
 	CodeVector extra;
 	CodeVector side;
+	uint32_t error;
 
 	std::unordered_map<uint32_t, int> codeCounts;
+};
+
+struct DeckLimits
+{
+	struct Boundary
+	{
+		std::size_t min, max;
+	}main{40, 60}, extra{0, 15}, side{0, 15};
 };
 
 } // namespace Multirole
