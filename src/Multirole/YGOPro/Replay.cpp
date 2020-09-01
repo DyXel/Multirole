639698a84194101ab4902f1b581d40edd6e78997
Dylam De La Torre <dyxel04@gmail.com>
2020-09-01 21:51:01 +0200

    when recording core messages filter out useless ones for replay

--- src/Multirole/YGOPro/Replay.cpp
+++ src/Multirole/YGOPro/Replay.cpp
@@ -26,8 +26,44 @@ void Replay::AddDuelist(uint8_t team, Duelist&& duelist)
 	duelists[team].emplace_back(duelist);
 }
 
-void Replay::RecordMsg(const STOCMsg& msg)
+void Replay::RecordMsg(const std::vector<uint8_t>& msg)
 {
+	// Filter out some useless messages.
+	switch(msg[0U])
+	{
+		case MSG_HINT:
+		{
+			switch(msg[1U])
+			{
+				// Do not record player specific hints.
+				case 1U: case 2U:
+				case 3U: case 5U:
+					return;
+			}
+		}
+		case MSG_SELECT_BATTLECMD:
+		case MSG_SELECT_IDLECMD:
+		case MSG_SELECT_EFFECTYN:
+		case MSG_SELECT_YESNO:
+		case MSG_SELECT_OPTION:
+		case MSG_SELECT_CHAIN:
+		case MSG_SELECT_PLACE:
+		case MSG_SELECT_DISFIELD:
+		case MSG_SELECT_POSITION:
+		case MSG_SELECT_COUNTER:
+		case MSG_SELECT_SUM:
+		case MSG_SORT_CARD:
+		case MSG_SORT_CHAIN:
+		case MSG_ROCK_PAPER_SCISSORS:
+		case MSG_ANNOUNCE_RACE:
+		case MSG_ANNOUNCE_ATTRIB:
+		case MSG_ANNOUNCE_CARD:
+		case MSG_ANNOUNCE_NUMBER:
+		case MSG_SELECT_CARD:
+		case MSG_SELECT_TRIBUTE:
+		case MSG_SELECT_UNSELECT_CARD:
+			return;
+	}
 	messages.emplace_back(msg);
 }
 
@@ -40,5 +76,30 @@ void Replay::Serialize()
 {
 
 }
+
+// ***** YRPX Binary format *****
+// ReplayHeader
+// team0Count [uint8_t]
+// team0Names [20 uint16_t * team0Count]
+// team1Count [uint8_t]
+// team1Names [20 uint16_t * team1Count]
+// duelFlags [uint32_t]
+// DECKS
+// Core messages: uint8_t msgType, followed by uint32_t length of message, followed by uint8_t * length
+//
+// OLD_REPLAY_FORMAT
+//
+// ReplayHeader
+// team0Count [uint8_t]
+// team0Names [20 uint16_t * team0Count]
+// team1Count [uint8_t]
+// team1Names [20 uint16_t * team1Count]
+// startingLP [uint32_t]
+// startingDrawCount [uint32_t]
+// drawCountPerTurn [uint32_t]
+// duelFlags [uint32_t]
+// DECKS
+// EXTRA_CARDS
+// Core responses: uint8_t length, followed by uint8_t * length
 
 } // namespace YGOPro
