3e591f3ab4061fa34c3d14664a7110e0d51f7bc8
edo9300 <edoardo762@gmail.com>
2020-09-30 15:46:01 +0200

    fix compilation of UTF conv functions for vs 15/17
    https://social.msdn.microsoft.com/Forums/en-US/8f40dcd8-c67f-4eba-9134-a19b9178e481/vs-2015-rc-linker-stdcodecvt-error?forum=vcgeneral
    https://stackoverflow.com/questions/32055357/visual-studio-c-2015-stdcodecvt-with-char16-t-or-char32-t
    https://stackoverflow.com/questions/38688417/utf-conversion-functions-in-c11

--- src/Multirole/YGOPro/StringUtils.cpp
+++ src/Multirole/YGOPro/StringUtils.cpp
@@ -20,6 +20,20 @@ std::u16string BufferToUTF16(const void* data, std::size_t maxByteCount)
 	return std::u16string(p2, p - p2);
 }
 
+#if defined(_MSC_VER) && _MSC_VER >= 1900 && _MSC_VER < 1920
+#define WCI std::wstring_convert<std::codecvt_utf8_utf16<int16_t>, int16_t>{}
+std::string UTF16ToUTF8(std::u16string_view str)
+{
+	auto p = reinterpret_cast<const int16_t*>(str.data());
+	return WCI.to_bytes(p, p + str.size());
+}
+
+std::u16string UTF8ToUTF16(std::string_view str)
+{
+	auto asInt = WCI.from_bytes(str.data(), str.data() + str.size());
+	return std::u16string(reinterpret_cast<char16_t const*>(asInt.data()), asInt.length());
+}
+#else
 #define WCI std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t>{}
 std::string UTF16ToUTF8(std::u16string_view str)
 {
@@ -30,6 +44,7 @@ std::u16string UTF8ToUTF16(std::string_view str)
 {
 	return WCI.from_bytes(str.data());
 }
+#endif
 #undef WCI
 
 } // namespace YGOPro
