87f99ab6fc60b4d096572e0151d74f744db40ad1
Dylam De La Torre <dyxel04@gmail.com>
2021-01-07 23:49:51 +0100

    handle std::wstring_convert::to_bytes throwing uncaught exception
    also remove macros and duplicated code because of visual studio poor standard
    support.

--- src/Multirole/YGOPro/StringUtils.cpp
+++ src/Multirole/YGOPro/StringUtils.cpp
@@ -20,31 +20,34 @@ std::u16string BufferToUTF16(const void* data, std::size_t maxByteCount)
 	return std::u16string(p2, p - p2);
 }
 
+
+constexpr const char* ERROR_STR = "Invalid String";
+
 #if defined(_MSC_VER) && _MSC_VER >= 1900 && _MSC_VER < 1920
-#define WCI std::wstring_convert<std::codecvt_utf8_utf16<int16_t>, int16_t>{}
-std::string UTF16ToUTF8(std::u16string_view str)
-{
-	auto p = reinterpret_cast<const int16_t*>(str.data());
-	return WCI.to_bytes(p, p + str.size());
-}
-
-std::u16string UTF8ToUTF16(std::string_view str)
-{
-	auto asInt = WCI.from_bytes(str.data(), str.data() + str.size());
-	return std::u16string(reinterpret_cast<char16_t const*>(asInt.data()), asInt.length());
-}
+using Wsc = std::wstring_convert<std::codecvt_utf8_utf16<int16_t>, int16_t>;
 #else
-#define WCI std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t>{}
+using Wsc = std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t>;
+#endif // defined(_MSC_VER) && _MSC_VER >= 1900 && _MSC_VER < 1920
+
+using WscElem = Wsc::wide_string::value_type;
+
+inline Wsc MakeWsc()
+{
+	return Wsc{ERROR_STR};
+}
+
 std::string UTF16ToUTF8(std::u16string_view str)
 {
-	return WCI.to_bytes(str.data());
+	const auto* p = reinterpret_cast<const WscElem*>(str.data());
+	return MakeWsc().to_bytes(p, p + str.size());
 }
 
 std::u16string UTF8ToUTF16(std::string_view str)
 {
-	return WCI.from_bytes(str.data());
+	if constexpr(std::is_same_v<char16_t, WscElem>)
+		return MakeWsc().from_bytes(str.data());
+	const auto r = MakeWsc().from_bytes(str.data(), str.data() + str.size());
+	return std::u16string(reinterpret_cast<const char16_t*>(r.data()), r.length());
 }
-#endif
-#undef WCI
 
 } // namespace YGOPro
