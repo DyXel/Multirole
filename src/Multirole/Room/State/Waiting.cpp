164e5bb60e6d91ae13b2baa64fa44f97839de156
Dylam De La Torre <dyxel04@gmail.com>
2021-06-12 18:15:40 +0200

    send spectator count to newly joined clients only if its non-0

--- src/Multirole/Room/State/Waiting.cpp
+++ src/Multirole/Room/State/Waiting.cpp
@@ -59,7 +59,8 @@ StateOpt Context::operator()(State::Waiting& s, const Event::Join& e)
 		SendToAll(MakePlayerEnter(e.client));
 		SendToAll(MakePlayerChange(e.client));
 		e.client.Send(MakeTypeChange(e.client, s.host == &e.client));
-		e.client.Send(MakeWatchChange(spectators.size()));
+		if(const auto specSize = spectators.size(); specSize > 0U)
+			e.client.Send(MakeWatchChange(specSize));
 		SendDuelistsInfo(e.client);
 	}
 	else
