9138e9bc5f460b9185acf5b4e5f8cb62cd0812a4
Dylam De La Torre <dyxel04@gmail.com>
2022-04-29 23:29:30 +0200

    move ellipsis to the echo function
    removes duplication :^)

--- util/area-zero.sh
+++ util/area-zero.sh
@@ -23,23 +23,23 @@
 MULTIROLE_PID=0
 
 ts_echo() {
-	date "+[%Y-%m-%d %T.???] $1"
+	date "+[%Y-%m-%d %T.???] $1..."
 }
 
 launch_multirole() {
-	ts_echo "Launching Multirole..."
+	ts_echo "Launching Multirole"
 	./multirole &
 	MULTIROLE_PID=$!
 }
 
 launch_multirole_if_not_running() {
 	kill -s 0 $MULTIROLE_PID 2>/dev/null && return
-	ts_echo "Multirole exited without my signaling! Relaunching..."
+	ts_echo "Multirole exited without my signaling! Relaunching"
 	launch_multirole
 }
 
 term_and_launch_multirole() {
-	ts_echo "Signaling Multirole..."
+	ts_echo "Signaling Multirole"
 	disown %%
 	kill $MULTIROLE_PID >/dev/null
 	launch_multirole
