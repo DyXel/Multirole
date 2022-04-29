060004b01f3c5cac94736dfaeb969eab88d81b50
Dylam De La Torre <dyxel04@gmail.com>
2022-04-29 23:28:29 +0200

    add script description (adapted from area-zero.py)

--- util/area-zero.sh
+++ util/area-zero.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+# area-zero.sh - Helper script for Multirole
+# ==========================================
+#
+# Runs the main server executable and manages its lifetime.
+#
+# When initially executed, this script will launch an instance of Multirole
+# and will wait until it exits (gracefully or not), after that, it will try
+# to launch another instance and wait for that one, repeating the process.
+#
+# A signal handler for SIGTERM is installed, when the signal is received it is
+# forwarded to the currently running server instance, and after a small grace
+# period this script will automatically launch another server executable,
+# resuming the waiting process described previously.
+#
+# This script is never meant to exit gracefully, therefore if you want to
+# terminate it, signal the process with SIGKILL (which cannot be caught),
+# this will relinquish the ownership of the currently running server instance
+# to the operating system. After that, it's just a matter of signaling Multirole
+# with SIGTERM so it can finish its execution gracefully.
+
 MULTIROLE_PID=0
 
 ts_echo() {
